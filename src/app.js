3ba14f8396e49e3d11a2d922057721902b72f012
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-03-21 00:44:36 +0530

    exploring get,post,delete calls

--- src/app.js
+++ src/app.js
@@ -4,18 +4,32 @@
  //creating new app by calling express func
  const app = express();
 
- //request handler
+//only handle Get call api to user
+app.get("/user" , (req,res)=>{
+    res.send({firstname:"Kirti" , lastname:"Sharma"})
+})
+
+//only handle Post call api to user
+app.post("/user",(req,res)=>{ 
+    res.send("Data saved to database successfully");
+})
+
+//only handle delete calls
+app.delete("/user",(req,res)=>{
+    res.send("Account deleted")
+})
+ //request handler : all http api calls
 app.use("/test" , (req,res)=>{
     res.send("hello from server")
 })
 
-app.use("/" , (req,res)=>{
-    res.send("this is wihtout slash route")
-})
+// app.use("/" , (req,res)=>{
+//     res.send("this is wihtout slash route")
+// })
 
-app.use("/hello" , (req,res)=>{
-    res.send("running at localhost 3000/help")
-})
+// app.use("/hello" , (req,res)=>{
+//     res.send("running at localhost 3000/help")
+// })
 
  app.listen(3000 , ()=>{
     console.log("Server has been started successfully")
